4fb49bca43a65dc5f3c5f40f13d5297fe4e477aa
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-06 21:43:59 -0700

    split and join

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str.split()[5:7]
+print(" ".join(str) +  " with python")
+
