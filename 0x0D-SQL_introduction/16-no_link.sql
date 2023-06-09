530e6a6389216f16a405360e162d1e9f73be5bdd
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-08 19:44:56 -0700

    a script that lists all records of the table second_table of the database

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- a script that lists all records of the table second_table of the database
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT DISTINCT score, name FROM second_table ORDER BY score DESC, name DESC;
