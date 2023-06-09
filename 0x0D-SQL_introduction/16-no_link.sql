d4e67cc629787d6dd4f186c686a093e11344f833
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-08 19:51:43 -0700

    kinda complicated

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- a script that lists all records of the table second_table of the database
-SELECT DISTINCT score, name FROM second_table ORDER BY score DESC, name DESC;
+SELECT DISTINCT score, name FROM second_table WHERE name IS NOT NULL  ORDER BY score DESC, name DESC;
