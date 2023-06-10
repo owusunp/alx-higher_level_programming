a45faf0700164c5154670a1940a7a40b99827b11
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-10 02:21:23 -0700

    a script that creates the MySQL server user user_0d_1.

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- a script that creates the MySQL server user user_0d_1.
+-- Run the following SQL query to create the user_0d_1 user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
