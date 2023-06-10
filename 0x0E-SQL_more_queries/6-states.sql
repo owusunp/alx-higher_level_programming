79f021f71b7400c46dbaf0f7f42372993c591b07
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-10 05:01:13 -0700

    id INT unique, auto generated, can’t be null and is a primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- id INT unique, auto generated, can’t be null and is a primarkey
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       name VARCHAR(256) NOT NULL);
+
