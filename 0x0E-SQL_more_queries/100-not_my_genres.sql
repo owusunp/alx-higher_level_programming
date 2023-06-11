dcb45fd43dccad11f78436fc3732798df8ce6eb7
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-11 15:27:35 -0700

    hard stuff

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,6 @@
 -- tv_shows table contains only one record where title = Dexter (but the id can be different
 -- table 1 tv_genres||tv_show_genres||tv_shows
+-- tv_shows table contains only one record where title = Dexter (but the id can be different
+-- table 1 tv_genres||tv_show_genres||tv_shows
 
-SELECT DISTINCT tv_genres.name AS name FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.id != 8 ORDER BY tv_genres.name; 
+SELECT DISTINCT tv_genres.name AS name FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.show_id !=  8 ORDER BY tv_genres.name; 
