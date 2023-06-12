9049e736f57f61ca8f7bac78bf0d59a2c73fbe4a
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-12 03:50:33 -0700

    note no

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         number = -number
-        print("{}".format(number % 10, end=""))
+        print("{:d}".format(number % 10, end=""))
     else:
         print("{}".format(number % 10, end=""))
+    return number % 10
