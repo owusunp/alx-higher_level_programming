120060b5289739cdb163e17ad322724a545687ef
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-12 03:22:49 -0700

    chr and ord

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str): 
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            upper_case = chr(ord(c) - 32)
+        else:
+            upper_case = c
+        print("{}".format(upper_case), end="")
+    print()
