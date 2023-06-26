819658b565bead64ef959b1b6fdd41af6061549b
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-26 01:03:36 -0700

    cant access sets with []

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-from functools import reduce
 def uniq_add(my_list=[]):
     new_list = set(my_list)
-    return reduce(lambda x, y: x + y, new_list)
+    sum = 0
+    for i in new_list:
+        sum += i
+    return sum
