3e19f630c79bd9ec8daae381d4ea23fa3471a6d7
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-26 02:01:55 -0700

    use sorted to sort dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     a_dictionary = dict(sorted(a_dictionary.items()))
     for i,v in a_dictionary.items():
-        print("{:s} : {}".format(i, v))
+        print("{:s}: {}".format(i, v))
 
