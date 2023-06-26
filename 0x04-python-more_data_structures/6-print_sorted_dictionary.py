6b40ce7c0c6589d56d0f3884a3ca7f9c8eabd4ae
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-26 02:00:33 -0700

    dict(sorted(hjk.items()))

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    a_dictionary = dict(sorted(a_dictionary.items()))
+    for i,v in a_dictionary.items():
+        print("{:s} : {}".format(i, v))
+
