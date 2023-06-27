c114594ec659aefef76fe1b2aaa9cc63dd43ee37
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-26 17:41:06 -0700

    valueeroor

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,11 @@
 def safe_print_list_integers(my_list=[], x=0):
     k = 0
     for i in range(x):
+        
         try:
             print("{:d}".format(my_list[i]), end="")
             k = k + 1
-        except ValueError:
+        except:
             continue
     print()
     return k
