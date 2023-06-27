0537d460c7f04c50b629857a6bd78c30dc04170e
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-27 01:00:36 -0700

    finally

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,6 +17,8 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result.append(0)
             continue
+        finally:
+            j = 0
     for i in range(list_length):
         list_last.append(result[i])
     return list_last
