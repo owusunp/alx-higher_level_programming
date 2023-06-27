6fe2ff6ab0900e3b59d80b070c41c9009aab0564
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-26 20:39:03 -0700

    cool stuff

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    list_last = []
+    for i in range(list_length):
+        try:
+            result.append(my_list_1[i]/my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+            continue
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+            continue
+        except IndexError:
+            print("out of range")
+            result.append(0)
+            continue
+    for i in range(list_length):
+        list_last.append(result[i])
+    return list_last
+
