4d8449a0ec199c9205e8f9812dc115ebfd77d767
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-29 05:20:36 -0700

    add up

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -69,7 +69,7 @@ class Rectangle:
     def perimeter(self):
         """ returns the perimeter """
         if self.__width == 0 or self.__height == 0:
-            return ""
+            return 0
         return 2*self.__width + 2*self.__height
 
     def __str__(self):
