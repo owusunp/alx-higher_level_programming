62f959d15d3aa598d816976faa69f45fdd8cff59
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-29 05:27:15 -0700

    dada

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -77,8 +77,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         fill_hash = ""
-        if self.__width == 0 or self.height == 0:
-            return 0
+        
         for i in range(self.__height - 1):
             for j in range(self.__width):
                 fill_hash += "#" 
