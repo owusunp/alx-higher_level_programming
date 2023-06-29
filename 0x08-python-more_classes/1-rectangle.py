4f3aa7f693805f8c27e96e2a3762ed092ffe7396
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-28 20:58:50 -0700

    width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" more on rectangle """
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        """this func acting as a constructor """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ returns width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width """
+        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """ returns height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ sets the height """
+        self.__height = value
+        
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("height must be >= 0")
+
+
+
+
