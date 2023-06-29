713a9ab9d259e4e1e546fb6bca59297c18afd458
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-29 04:51:33 -0700

    update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,8 +3,8 @@
 class Rectangle:
     def __init__(self, width=0, height=0):
         """this func acting as a constructor """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
