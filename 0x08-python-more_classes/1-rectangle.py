20b2b1c8f121134d1d84b4ccac1c62f6eb0b3995
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-06-29 03:41:50 -0700

    workgit add .

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,43 +1,74 @@
 #!/usr/bin/python3
-""" more on rectangle """
+""" ABOUT TO DO SOME SERIOUS CODING HERE """
 
 
 class Rectangle:
-    """ lets so more codes in this class"""
+    """A class representing a rectangle."""
+
     def __init__(self, width=0, height=0):
-        """this func acting as a constructor """
+        """Initialize the Rectangle object.
+
+        Args:
+            width (int): The width of the rectangle. Defaults to 0.
+            height (int): The height of the rectangle. Defaults to 0.
+        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """ returns width """
+        """Get the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """sets the width """
-        
-        self.__width = value
+        """Set the width of the rectangle.
+
+        Args:
+            value (int): The width value to set.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
         if isinstance(value, int):
             if value < 0:
                 raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
         else:
             raise TypeError("width must be an integer")
 
     @property
     def height(self):
-        """ returns height """
+        """Get the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ sets the height """
-    
-        self.__height = value
+        """Set the height of the rectangle.
+
+        Args:
+            value (int): The height value to set.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
         if isinstance(value, int):
             if value < 0:
-                raise ValueError("height must be >=0")
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
         else:
             raise TypeError("height must be an integer")
-        
+
+
