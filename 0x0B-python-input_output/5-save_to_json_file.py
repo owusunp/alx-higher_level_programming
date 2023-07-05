c7865ddeb90cbe19cce8b35a718945a4bef35bee
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-07-05 12:12:54 -0700

    json rep

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 
+""" json represenation """
+
 import json
 
 def save_to_json_file(my_obj, filename):
-    
-    with open(filename, "w", encoding = 'utf-8') as json_write:
+    """
+    Serializes and saves a Python object to a JSON file.
+
+    Args:
+        my_obj (Any): The Python object to be serialized and saved.
+        filename (str): The name of the file to save the JSON data.
+
+    Usage:
+        data = {"name": "John Doe", "age": 30}
+        save_to_json_file(data, "output.json")  # Serializes and saves the Python object to a JSON file.
+
+    """
+    with open(filename, "w", encoding='utf-8') as json_write:
         json.dump(my_obj, json_write)
+
