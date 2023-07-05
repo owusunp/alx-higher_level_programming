deaf3c10d5a4c12358827838fd1760696767d7df
owusunp <prince.owusu-nkrumah@vanderbilt.edu>
2023-07-05 12:57:36 -0700

    json to ibj

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 
+
+""" convert the json string to object """
+
 import json
 
 def load_from_json_file(filename):
+    """
+    Loads JSON data from a file and returns the corresponding Python object.
+
+    Args:
+        filename (str): The name of the JSON file to load.
+
+    Returns:
+        The Python object representing the JSON data.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        JSONDecodeError: If the JSON data in the file is not valid.
+
+    Example:
+        >>> data = load_from_json_file("data.json")
+    """
     with open(filename) as json_obj:
         return json.load(json_obj)
 
