51abcef14d04a26fe50b5d0e779f3805098cb702
owusunp <prince.yafgh@gmail.com>
2023-07-20 08:21:17 +0000

    argv processs

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let argLen = process.argv.length - 2;
+if (argLen === 0){
+console.log("No argument");
+}
+else if(argLen === 1){
+console.log("Argument found")
+}
+else{
+console.log("Arguments found")
+
+
